0fc36be699b2fd70626b76906b098ed1d7600803
user7d11 <user7w11@gmail.com>
2022-07-20 13:21:56 +0200

    pics fixed, slide, change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'firstPageAngular';
+
+export class AppComponent implements OnInit {
+  images = ['gamer.png', 'landscape.png', 'paperRocket.png'];
+  currentImage = 0;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    }, 8000);
+
+  }
+
 }
