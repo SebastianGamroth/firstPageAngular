bf9f592c747db63e4d53e0de91317f257e868253
user7d11 <user7w11@gmail.com>
2022-07-20 15:28:17 +0200

    Angular Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-  images = ['gamer.png', 'landscape.png', 'paperRocket.png'];
-  headlines = [
-    'Bring engineering to the next lewel',
-    'Born to code',
-    'Graduated at TUM'
-  ];
+export class AppComponent {
 
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-
-    }, 8000);
-
-  }
 
 }
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+
+  images = ['gamer.png', 'landscape.png', 'paperRocket.png'];
+  headlines = [
+    'Bring engineering to the next lewel',
+    'Born to code',
+    'Graduated at TUM'
+  ];
+
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+
+    }, 8000);
+
+  }
+
+}
